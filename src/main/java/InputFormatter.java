970ddb523d3bbc69e00a4a74d6aa7525a6a7d7e8
Md Jubayer <jubayer.sust.23@gmail.com>
2021-10-08 00:56:31 +0100

    Modify the "formatUserValidInput" method to pass the unit test when input string contains only single digit.

--- src/main/java/InputFormatter.java
+++ src/main/java/InputFormatter.java
@@ -3,6 +3,11 @@ public class InputFormatter {
     public Long formatUserValidInput(String validInput) {
         long formattedValue = 0;
 
+        try {
+            formattedValue = Long.parseLong(validInput);
+        } catch (NumberFormatException e) {
+            formattedValue = 0;
+        }
 
         return formattedValue;
     }
