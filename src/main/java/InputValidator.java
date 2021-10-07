ff5cef2ef2decd3a2f7e40a62139192578b6e478
Md Jubayer <jubayer.sust.23@gmail.com>
2021-10-08 00:20:00 +0100

    Modify "isValidInput" method to pass the Unit test for alphabet only input.

--- src/main/java/InputValidator.java
+++ src/main/java/InputValidator.java
@@ -2,12 +2,18 @@ import java.util.regex.Pattern;
 
 public class InputValidator {
 
+    private final String input_regex = "[0-9]*";
+
     public boolean isValidInput(String input) {
 
         boolean isValid = true;
 
         if (input.isEmpty())
             isValid = false;
+
+        else if(!Pattern.matches(input_regex, input)){
+            isValid = false;
+        }
 
         return isValid;
     }
