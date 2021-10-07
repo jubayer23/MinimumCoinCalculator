b53c0aa5ceff91ed06e228d3f96b2bb73fcdcf71
Md Jubayer <jubayer.sust.23@gmail.com>
2021-10-08 00:12:07 +0100

    Create the method "isValidInput" which pass the Unit test for blank input

--- src/main/java/InputValidator.java
+++ src/main/java/InputValidator.java
@@ -1,2 +1,14 @@
+import java.util.regex.Pattern;
+
 public class InputValidator {
+
+    public boolean isValidInput(String input) {
+
+        boolean isValid = true;
+
+        if (input.isEmpty())
+            isValid = false;
+
+        return isValid;
+    }
 }
