06598ed63ebef800ab7932b330fb7c1aaae9a22a
Md Jubayer <jubayer.sust.23@gmail.com>
2021-10-08 01:08:44 +0100

    Modify the "formatUserValidInput" method in order to pass all the Unit test created in previous steps.

--- src/main/java/InputFormatter.java
+++ src/main/java/InputFormatter.java
@@ -1,14 +1,36 @@
 public class InputFormatter {
 
+    /*
+    *  A method to format user valid input data to the corresponding pence value.
+    * */
     public Long formatUserValidInput(String validInput) {
         long formattedValue = 0;
+        boolean hasPoundSign = validInput.contains("£");
+        validInput = validInput.replaceFirst("£", "");
+        validInput = validInput.replaceFirst("p", "");
 
-        try {
-            formattedValue = Long.parseLong(validInput);
-        } catch (NumberFormatException e) {
-            formattedValue = 0;
+
+        boolean hasDecimalPoint = validInput.contains(".");
+        if (hasDecimalPoint) {
+
+
+            formattedValue = Math.round(Double.parseDouble(validInput) * 100.0);
+
+        } else {
+
+
+            if (hasPoundSign) {
+                validInput = validInput + "00";
+            }
+
+            try {
+                formattedValue = Long.parseLong(validInput);
+            } catch (NumberFormatException e) {
+                formattedValue = 0;
+            }
         }
 
         return formattedValue;
     }
+
 }
--- src/main/java/InputValidator.java
+++ src/main/java/InputValidator.java
@@ -1,9 +1,16 @@
 import java.util.regex.Pattern;
 
+
 public class InputValidator {
 
+    /*
+    *  Regular expression to filtered out all the invalid string input
+    * */
     private final String input_regex = "£?\\d[0-9]*\\.?[0-9]*p?P?";
 
+    /*
+     *  A Method to check whether user input data is valid or not.
+     * */
     public boolean isValidInput(String input) {
 
         boolean isValid = true;
