862bfe115b161145d798e1e0c083db6bc3fade09
Md Jubayer <jubayer.sust.23@gmail.com>
2021-10-08 01:28:08 +0100

    Modify the "CalculateMinCoinDenominations" method to make the code more concise and readable and get rid of if-else conditions.

--- src/main/java/Model/Amount.java
+++ src/main/java/Model/Amount.java
@@ -21,71 +21,25 @@ public class Amount {
         this.formattedAmount = formattedAmount;
     }
 
+    /*
+    *  A method to calculate the minimum number of Sterling coins equivalent to that amount.
+    * */
     public String CalculateMinCoinDenominations() {
+        int[] coins = {200, 100, 50, 20, 10, 5, 2, 1};
+        String[] coins_map = {"£2", "£1", "50p", "20p", "10p", "5p", "2p", "1p"};
+
         String result = "";
 
-        String pound = "£";
-        if (formattedAmount >= 200) {
-            //System.out.print((validInput / 200) + " X 2" + pound);
-            result = result + (formattedAmount / 200) + " X 2" + pound;
-            formattedAmount = formattedAmount % 200;
-            if (formattedAmount > 0) result = result + ", ";
+
+        for (int i = 0; i < coins.length; i++) {
+            if (formattedAmount >= coins[i]) {
+                //System.out.print((validInput / 200) + " X 2" + pound);
+                result = result + (formattedAmount / coins[i]) + " x " + coins_map[i];
+                formattedAmount = formattedAmount % coins[i];
+                if (formattedAmount > 0) result = result + ", ";
+            }
         }
 
-        if (formattedAmount >= 100) {
-
-
-            result = result + (formattedAmount / 100) + " X 1" + pound;
-            formattedAmount = formattedAmount % 100;
-            if (formattedAmount > 0) result = result + ", ";
-        }
-
-        if (formattedAmount >= 50) {
-
-
-            result = result + (formattedAmount / 50) + " X 50p";
-            formattedAmount = formattedAmount % 50;
-            if (formattedAmount > 0) result = result + ", ";
-        }
-
-        if (formattedAmount >= 20) {
-
-
-            result = result + (formattedAmount / 20) + " X 20p";
-            formattedAmount = formattedAmount % 20;
-            if (formattedAmount > 0) result = result + ", ";
-        }
-
-        if (formattedAmount >= 10) {
-
-
-            result = result + (formattedAmount / 10) + " X 10p";
-            formattedAmount = formattedAmount % 10;
-            if (formattedAmount > 0) result = result + ", ";
-        }
-
-        if (formattedAmount >= 5) {
-
-
-            result = result + (formattedAmount / 5) + " X 5p";
-            formattedAmount = formattedAmount % 5;
-            if (formattedAmount > 0) result = result + ", ";
-        }
-
-        if (formattedAmount >= 2) {
-
-
-            result = result + (formattedAmount / 2) + " X 2p";
-            formattedAmount = formattedAmount % 2;
-            if (formattedAmount > 0) result = result + ", ";
-        }
-
-        if (formattedAmount >= 1) {
-
-
-            result = result + (formattedAmount / 1) + " X 1p";
-            formattedAmount = 0L;
-        }
 
         return result;
     }
